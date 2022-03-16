0404533ae48f8485b937fe03e495a4361af190ed
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-16 10:38:18 +0530

    js code callback delay value changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,70 +15,68 @@ let boring_text = document.querySelector('.boring-text'),
     hat = document.querySelector('.hat'),
     mayo_bottle = document.querySelector('.mayo-bottle');
 
-setTimeout(()=>{
+setTimeout(() => {
     plate.style.transform = "translateX(0)";
-    setTimeout(()=>{
+    setTimeout(() => {
         sad_sonali.style.transform = "translateX(0)";
         boring_text.style.transform = "translateX(0)";
-        setTimeout(()=>{
+        setTimeout(() => {
             plate.style.transform = "translateX(-500px)";
-        },1600);
-        setTimeout(()=>{
+        }, 1600);
+        setTimeout(() => {
             sad_sonali.style.transform = "translateX(500px)";
             boring_text.style.transform = "translateX(-500px)";
             sandwich1.style.transform = "translateX(0)";
             sandwich2.style.transform = "translateX(0)";
             sandwich3.style.transform = "translateX(0)";
-            setTimeout(()=>{
+            setTimeout(() => {
                 sandwich3.style.bottom = "12%";
                 knife.style.transform = "translateX(0)";
-                setTimeout(()=>{
+                setTimeout(() => {
                     knife.style.left = "40%";
-                    setTimeout(()=>{
                     mayo.style.transform = "translateX(0)";
-                     knife.style.display = "none";  
-                     setTimeout(()=>{
-                        sandwich3.style.bottom = "8%";
-                        mayo.style.zIndex = "0";
-                        setTimeout(()=>{
-                            sandwicheto_text.style.transform = "translateX(0)";
-                            happy_sonali.style.transform = "translateX(0)";
-                            setTimeout(()=>{
-                                sandwicheto_text.style.transform = "translateX(-500px)";
-                                happy_sonali.style.transform = "translateX(500px)";
-                                setTimeout(()=>{
-                                    funfoods_text.style.transform = "translateX(0)";
-                                    chef.style.transform = "translateX(0)";
-                                    setTimeout(()=>{
-                                        funfoods_text.style.transform = "translateX(-500px)";
-                                        chef.style.transform = "translateX(500px)";
-                                        sandwich1.style.transform = "translateX(-500px)";
-                                        sandwich2.style.transform = "translateX(-500px)";
-                                        sandwich3.style.transform = "translateX(-500px)";
-                                        mayo.style.transform = "translateX(-500px)";
-                                        setTimeout(()=>{
-                                            mayo_bottle.style.transform = "translateX(0)";
-                                            foodmagic_text.style.transform = "translateX(0)";
-                                            setTimeout(()=>{
-                                                hat.style.transform = "translateX(0)";
-                                                setTimeout(()=>{
-                                                    hat.style.top = "60%";
-                                                    setTimeout(()=>{
-                                                        recipe.style.transform = "translate(0)";
-                                                    },2000);
-                                                },2000)
-                                            },2000);
-                                        },1000);
-                                    },2000);
-                                },500);
-                                
-                            },1000);
-                        },1500);
-                     },100) 
-                    },50);
-                },1000);
-            },2000);
-        },2000);
-    },2000);
-},2000);
-
+                    setTimeout(() => {
+                        knife.style.display = "none";
+                        setTimeout(() => {
+                            sandwich3.style.bottom = "8%";
+                            mayo.style.zIndex = "0";
+                            setTimeout(() => {
+                                sandwicheto_text.style.transform = "translateX(0)";
+                                happy_sonali.style.transform = "translateX(0)";
+                                setTimeout(() => {
+                                    sandwicheto_text.style.transform = "translateX(-500px)";
+                                    happy_sonali.style.transform = "translateX(500px)";
+                                    setTimeout(() => {
+                                        funfoods_text.style.transform = "translateX(0)";
+                                        chef.style.transform = "translateX(0)";
+                                        setTimeout(() => {
+                                            funfoods_text.style.transform = "translateX(-500px)";
+                                            chef.style.transform = "translateX(500px)";
+                                            sandwich1.style.transform = "translateX(-500px)";
+                                            sandwich2.style.transform = "translateX(-500px)";
+                                            sandwich3.style.transform = "translateX(-500px)";
+                                            mayo.style.transform = "translateX(-500px)";
+                                            setTimeout(() => {
+                                                mayo_bottle.style.transform = "translateX(0)";
+                                                foodmagic_text.style.transform = "translateX(0)";
+                                                setTimeout(() => {
+                                                    hat.style.transform = "translateX(0)";
+                                                    setTimeout(() => {
+                                                        hat.style.top = "60%";
+                                                        setTimeout(() => {
+                                                            recipe.style.transform = "translate(0)";
+                                                        }, 1500);
+                                                    }, 2000)
+                                                }, 2000);
+                                            }, 1000);
+                                        }, 2000);
+                                    }, 2000);
+                                }, 2000);
+                            }, 1500);
+                        }, 100)
+                    }, 50);
+                }, 1000);
+            }, 2000);
+        }, 2000);
+    }, 2000);
+}, 2000);
